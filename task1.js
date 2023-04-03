a409dc8e46de4c2f88a820e9e4d86739c69f6a18
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-04 05:15:50 +0900

    first commit

---
+++ task1.js
@@ -0,0 +1,62 @@
+
+//===============================================
+//Find odd numbers
+
+// function even_or_odd(number) {
+//   return number % 2 ? "Odd" : "Even"
+// }
+
+//================================================
+
+
+// function multiply(a, b){
+//  return a * b
+// }
+
+
+//================================================
+//Only positive numbers
+
+
+// let key = [20,30,20,-30]
+
+// const positiveSum = arr => key.filter(value => value > 0).reduce((a, b) => a + b, 0);
+
+
+// // positiveSum(20,30,-40)
+// console.log(positiveSum());
+
+
+
+
+
+//==================================================
+//Negative
+
+// function makeNegative(num) {
+//     return  num < 0 ? num : -num
+//    }
+
+//    console.log(makeNegative(10));
+
+
+
+//===================================================
+//Opposite
+
+// let num = 200
+
+// console.log(-num);
+
+
+//====================================================
+//Convert boolean values to strings 'Yes' or 'No'.
+
+let bool1 = 8
+
+function boolToWord( bool ){
+    return bool1 === true || bool1 >= 0 ? 'Yes,You are right' : 'No,Please...'
+   }
+
+
+console.log(boolToWord());
